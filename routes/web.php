0ba4033b2bbf91fbadd739649608b10ef1155c5b
nisa <yuannisa24si@mahasiswa.pcr.ac.id>
2025-10-02 10:56:04 +0700

    buat form

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,3 +29,6 @@ Route::get('/about', function () {
 })->name('mahasiswa.tampilan');
 
 Route:: get('/home', [HomeController::class,'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
